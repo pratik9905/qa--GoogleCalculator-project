a270a9fa42b8ce8a46ee4519fc88f630703029fe
Abhinav Srivastava <pratikkumar734@gmail.com>
2024-02-07 02:35:36 +0530

    ME_QA_XGOOGLE_CALC1_MODULE_L1

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -2,13 +2,18 @@ package demo;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+
+import dev.failsafe.internal.util.Assert;
+
 import java.util.logging.Level;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import net.bytebuddy.asm.Advice.This;
 
 
 public class TestCases {
@@ -49,7 +54,117 @@ public class TestCases {
     public  void testCase01(){
         System.out.println("Start Test case: testCase01");
         driver.get("https://www.google.com");
+        driver.findElement(By.id("APjFqb")).clear();
+        driver.findElement(By.id("APjFqb")).sendKeys("calculator");
+        driver.findElement(By.className("gNO89b")).click();
+        String actual = driver.findElement(By.id("cwos")).getText();
+        String expected = "0";
+        if(actual.equals(expected)){
+            System.out.println(actual);
+        }
+
+
+        System.out.println("end Test case: testCase01");
+    }
+
+    public void testCase02() throws InterruptedException{
+        System.out.println("Start Test case: testCase02");
+
+        driver.findElement(By.xpath("//div[@role='button'][normalize-space()='5']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@role='button'][normalize-space()='+']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@role='button'][normalize-space()='7']")).click();
+        Thread.sleep(2000);
+        //div[@role='button'][normalize-space()='=']
+        driver.findElement(By.xpath("//div[@role='button'][normalize-space()='=']")).click();
+        Thread.sleep(2000);
+        String actual = driver.findElement(By.id("cwos")).getText();
+        String expected = "12";
+        if(actual.equals(expected)){
+            System.out.println(actual);
+        }
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//div[@role='button'][normalize-space()='1']")).click();
+        driver.findElement(By.xpath("//div[@role='button'][normalize-space()='5']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@aria-label='minus']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@role='button'][normalize-space()='8']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@role='button'][normalize-space()='=']")).click();
+        Thread.sleep(2000);
+        String actualSub = driver.findElement(By.id("cwos")).getText();
+        String expectedSub = "7";
+        if(actualSub.equals(expectedSub)){
+            System.out.println(actualSub);
+        }
+        Thread.sleep(2000);
+
+
+
+
         System.out.println("end Test case: testCase02");
+
+    }
+
+    public void testCase03() throws InterruptedException{
+        System.out.println("Start Test case: testCase03");
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//div[@role='button'][normalize-space()='1']")).click();
+        driver.findElement(By.xpath("//div[@role='button'][normalize-space()='0']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@aria-label='multiply']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@role='button'][normalize-space()='3']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@role='button'][normalize-space()='=']")).click();
+        Thread.sleep(2000);
+        String actualMul = driver.findElement(By.id("cwos")).getText();
+        String expectedMul = "30";
+        if(actualMul.equals(expectedMul)){
+            System.out.println(actualMul);
+        }
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//div[@role='button'][normalize-space()='AC']")).click();
+        Thread.sleep(2000);
+        String actual = driver.findElement(By.id("cwos")).getText();
+        String expected = "0";
+        if(actual.equals(expected)){
+            System.out.println(actual);
+        }
+
+        
+
+
+        System.out.println("end Test case: testCase03");
+    }
+
+    public void testCase04() throws InterruptedException{
+        System.out.println("Start Test case: testCase03");
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//div[@role='button'][normalize-space()='2']")).click();
+        driver.findElement(By.xpath("//div[@role='button'][normalize-space()='0']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@aria-label='divide']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@role='button'][normalize-space()='4']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@role='button'][normalize-space()='=']")).click();
+        Thread.sleep(2000);
+        String actualdiv = driver.findElement(By.id("cwos")).getText();
+        String expectedDiv = "5";
+        if(actualdiv.equals(expectedDiv)){
+            System.out.println(actualdiv);
+        }
+        Thread.sleep(2000);
+
+
+        System.out.println("end Test case: testCase04");
     }
 
 
